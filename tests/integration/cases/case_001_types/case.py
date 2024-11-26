eb0735c5c79e3b6fb5cdd1503ccdb4ee6779ae65
zerlok <danil.troshnev@gmail.com>
2024-11-26 14:42:29 +0100

    add descriptor integration test case

--- tests/integration/cases/case_001_types/case.py
+++ tests/integration/cases/case_001_types/case.py
@@ -1,4 +1,15 @@
 from pyprotostuben.codegen.mypy.plugin import MypyStubProtocPlugin
 from tests.integration.cases.case import DirCaseProvider
 
-mypy_case = DirCaseProvider(__file__, MypyStubProtocPlugin(), "no-parallel")
+mypy_case = DirCaseProvider(
+    __file__,
+    MypyStubProtocPlugin(),
+    parameter="no-parallel",
+    expected_gen_source="expected_gen",
+)
+mypy_case_with_descriptors = DirCaseProvider(
+    __file__,
+    MypyStubProtocPlugin(),
+    parameter="no-parallel,include-descriptors",
+    expected_gen_source="expected_gen_descriptors",
+)
